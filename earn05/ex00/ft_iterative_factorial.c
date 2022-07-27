008520211f7b19bd6ab9470687a8818e349ab2e0
klertrat <110088388+klertrat@users.noreply.github.com>
2022-07-27 14:20:24 +0700

    Add files via upload
    next prime time out

---
+++ earn05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klertrat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/23 15:39:10 by klertrat          #+#    #+#             */
+/*   Updated: 2022/07/26 10:40:46 by klertrat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdio.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	result = 1;
+	if (nb == 0)
+		return (1);
+	else if (nb > 0)
+	{
+		while (nb >= 1)
+		{
+			result = result * nb;
+			nb--;
+		}
+		return (result);
+	}
+	return (0);
+}
+/*
+int	main(void)
+{
+	printf("%d", ft_iterative_factorial(13));
+}*/
---
+++ earn05/ex04/ft_fibonacci.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klertrat <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/23 15:46:12 by klertrat          #+#    #+#             */
+/*   Updated: 2022/07/26 15:00:39 by klertrat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdio.h>
+
+int	ft_fibonacci(int index)
+{
+	int	r;
+
+	if (index >= 0)
+	{
+		if (index == 0)
+			r = 0;
+		else if (index == 1)
+			r = 1;
+		else
+			r = ft_fibonacci(index - 1) + ft_fibonacci(index - 2);
+		return (r);
+	}
+	return (-1);
+}
+/*
+int	main(void)
+{
+	printf("%d",ft_fibonacci(5));
+}*/
